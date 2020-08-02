aa0f4b6e7050bdc464df369a6390b35c2dbcc7c6
songbb <2521728972@qq.com>
2020-08-02 11:30:58 +0800

    feat:新增文件上传功能

--- src/controllers/markdown.ts
+++ src/controllers/markdown.ts
@@ -1,10 +1,18 @@
 import { Controller, Post } from '../router'
 import { Context } from 'koa'
+import fs from 'fs'
+import path from 'path'
 
 @Controller('/api')
 export default class MarkDown {
     @Post()
     async uploadImage(ctx: Context): Promise<void> {
-        ctx.send(null, '上传成功')
+        const file = ctx.request.files.image
+        const reader = fs.createReadStream(file.path)
+        const filePath =
+            path.join(__dirname, '../public/upload') + `/${file.name}`
+        const upStream = fs.createWriteStream(filePath)
+        reader.pipe(upStream)
+        ctx.send(`http://localhost:5442/upload/${file.name}`, '上传成功')
     }
 }
